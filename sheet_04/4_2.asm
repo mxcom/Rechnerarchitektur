673c7df5378e3befa512712a3246ae1e6b6efa3e
mxcom <mxprogramming@protonmail.com>
2023-05-23 09:21:32 +0200

    Reformat solutions

--- sheet_04/4_2.asm
+++ sheet_04/4_2.asm
@@ -3,11 +3,22 @@
 	addi $9,$0,11
 	addi $10,$0,0x1000
 	addi $11,$0,-1
-	addi $12,$0,-0x8000
-	ori $13,$0,0x8000
-	lui $14,0xffff		# GPR[14]
-	ori $14, $14, 0x0000
-	lui $15,0x7fff		# GPR[15]
-	ori $15, $15, 0xffff
-	addi $24,$0,5322	# GPR[24]
+	addi $12,$0,-0x8000	#  Zweierkomplement: 0x8000 ->         1000 0000 0000 0000
+				#                                      0111 1111 1111 1111 (+1)
+				# 			               1000 0000 0000 0000
+				#  Sign Extension: 1111 1111 1111 1111 1000 0000 0000 0000
+				#                  f    f    f    f    8    0    0    0
+	
+	ori $13,$0,0x8000	# 0x8000 wird bei ori nicht als Zweierkomplement interpretiert
+	
+	lui $14,0xffff		# Setzt obere Haelfte des Zielregisters (16 Bits von 32 Bits) auf 0xffff
+	
+	lui $15,0x7fff		# Obere Haelfte auf 0x7fff setzen
+	ori $15,$15,0xffff	# Untere Haelfte durch OR Operation auf 0xffff setzen:
+				#    7fff0000
+				#    0000ffff
+				# OR --------
+				#    7fffffff
+	
+	addi $24,$0,5322
 	addi $25,$0,75
--- sheet_04/4_3.asm
+++ sheet_04/4_3.asm
@@ -3,13 +3,12 @@
 	addi $9,$0,11
 	addi $10,$0,0x1000
 	addi $11,$0,-1
-	addi $12,$0,-0x8000
+	addi $12,$0,-0x8000	
 	ori $13,$0,0x8000
-	lui $14,0xffff		# GPR[14]
-	ori $14, $14, 0x0000
-	lui $15,0x7fff		# GPR[15]
-	ori $15, $15, 0xffff
-	addi $24,$0,5322	# GPR[24]
+	lui $14,0xffff
+	lui $15,0x7fff
+	ori $15,$15,0xffff
+	addi $24,$0,5322
 	addi $25,$0,75
 	
 	# ----
@@ -17,7 +16,7 @@
 	add $2,$10,$9
 	sub $3,$10,$9
 	
-	nor $4,$0,$9
+	nor $4,$0,$9	# Komplement von $9 bilden und in $4 speichern
 	add $4,$4,$10
 	addi $4,$4,1
 	
