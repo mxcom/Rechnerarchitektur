1025affb1a5656269ed98538e18b84770e1638db
mxcom <mxprogramming@protonmail.com>
2023-05-23 08:50:36 +0200

    Add Solutions Sheet 4 Exercise 4.3

--- sheet_04/4_3.asm
+++ sheet_04/4_3.asm
@@ -11,3 +11,33 @@
 	ori $15, $15, 0xffff
 	addi $24,$0,5322	# GPR[24]
 	addi $25,$0,75
+	
+	# ----
+	
+	add $2,$10,$9
+	sub $3,$10,$9
+	
+	nor $4,$0,$9
+	add $4,$4,$10
+	addi $4,$4,1
+	
+	sll $5,$13,5
+	sra $6,$13,5
+	sra $7,$12,5
+	srl $16,$12,5
+	srlv $17,$13,$9
+	
+	div $24,$25	# Quotient gespeichert in LO Register
+			#     Rest gespeichert in HI Register
+	mflo $18	# move from lo: Kopiert Inhalt von LO Register (Quotient) in $18
+	
+	mfhi $19	# move from hi: Kopiert Inhalt von HI Register (Rest) in $19
+	
+	addiu $20,$15,1
+	
+	addi $21,$15,1
+	
+	clo $22,$11
+	clo $23,$14
+	
+	
